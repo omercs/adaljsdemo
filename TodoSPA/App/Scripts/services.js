acd1d48ae90e30306048795e49c2d640bab83a87
Omer Cansizoglu <cansizoc@gmail.com>
2015-03-20 10:52:06 -0700

    add service file

---
+++ TodoSPA/App/Scripts/services.js
@@ -0,0 +1,24 @@
+﻿var phonecatServices = angular.module('phonecatControllers', []);
+
+phonecatServices.factory('phoneListSvc', ['$http', function ($http) {
+    return {
+        getItems : function(){
+            return $http.get('/api/PhoneList');
+        },
+        getItem : function(id){
+            return $http.get('/api/PhoneList/' + id);
+        },
+        postItem : function(item){
+            return $http.post('/api/PhoneList/', item);
+        },
+        putItem : function(item){
+            return $http.put('/api/PhoneList/', item);
+        },
+        deleteItem : function(id){
+            return $http({
+                method: 'DELETE',
+                url: '/api/PhoneList/' + id
+            });
+        }
+    };
+}]);
