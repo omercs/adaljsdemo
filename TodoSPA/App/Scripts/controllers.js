168e208ca9e4ce3d77a5ff3268c7223969a8b8e9
Omer Cansizoglu <cansizoc@gmail.com>
2015-03-20 09:22:11 -0700

    model-view binding

---
+++ TodoSPA/App/Scripts/controllers.js
@@ -0,0 +1,24 @@
+﻿var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+
+    $scope.phones = [
+     {
+         'name': 'Nexus 5',
+         'snippet': 'Fast just got faster with Nexus 5',
+         'age': 1
+     },
+     {
+         'name': 'Motorola A',
+         'snippet': 'The Next, Next Generation tablet.',
+         'age': 2
+     },
+     {
+         'name': 'MOTOROLA B™',
+         'snippet': 'The Next, Next Generation tablet.',
+         'age': 3
+     }
+    ];
+
+    $scope.orderProp = 'age';
+});
