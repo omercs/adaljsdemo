a63d889fe8b0cb8de067a24c55fa30009f7843b6
Omer Cansizoglu <cansizoc@gmail.com>
2015-03-20 10:06:08 -0700

    router setup

---
+++ TodoSPA/App/Scripts/app.js
@@ -0,0 +1,24 @@
+﻿var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'App/Views/home.html',
+            controller: 'HomeCtrl'
+        }).
+        when('/phones', {
+            templateUrl: 'App/Views/phoneList.html',
+            controller: 'PhoneListCtrl'
+        }).
+        when('/phones/:phoneId', {
+            templateUrl: 'App/Views/phoneDetail.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
--- TodoSPA/App/Scripts/controllers.js
+++ TodoSPA/App/Scripts/controllers.js
@@ -1,19 +1,22 @@
-﻿var phonecatApp = angular.module('phonecatApp', []);
+﻿var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+phonecatControllers.controller('PhoneListCtrl', function ($scope) {
 
     $scope.phones = [
      {
+         'id': 1,
          'name': 'Nexus 5',
          'snippet': 'Fast just got faster with Nexus 5',
          'age': 1
      },
      {
+         'id': 2,
          'name': 'Motorola A',
          'snippet': 'The Next, Next Generation tablet.',
          'age': 2
      },
      {
+         'id': 3,
          'name': 'MOTOROLA B™',
          'snippet': 'The Next, Next Generation tablet.',
          'age': 3
@@ -22,3 +25,14 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
 
     $scope.orderProp = 'age';
 });
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function ($scope, $routeParams) {
+      $scope.phoneId = $routeParams.phoneId;
+  }]);
+
+phonecatApp.controller('HomeCtrl', function ($scope) {
+
+    $scope.message = "hello world";
+});
+
